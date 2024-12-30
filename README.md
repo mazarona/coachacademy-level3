32600a498bc3a192bfe11a6dcdbc8f09573389ca
mazarona <mazenfahem@gmail.com>
2024-12-30 05:54:42 +0200

    solve 2 problem dfs

---
+++ README.md
@@ -0,0 +1,8 @@
+# Topics
+
+- DFS
+- BFS
+- 
+- 
+
+
---
+++ dfs/3-pick-up-sticks.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define IO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+typedef long long ll;
+
+
+const int N = 1e6+1;
+vector<int> adj[N];
+bool visited[N];
+
+
+int main(){
+  IO;
+  int u, v; cin >> u >> v;
+
+  
+
+  return 0;
+}
+
